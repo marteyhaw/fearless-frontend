cd93c65daf742cbc4fee4745457a3fbf6de1405c
Martey Haw <martey.haw@gmail.com>
2022-11-23 15:33:04 -0800

    New Presentation stretch goal

---
+++ ghi/js/new-presentation.js
@@ -0,0 +1,37 @@
+window.addEventListener("DOMContentLoaded", async () => {
+  const url = "http://localhost:8000/api/conferences/";
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const data = await response.json();
+    const selectTag = document.getElementById("conference");
+
+    for (let conference of data.conferences) {
+      const option = document.createElement("option");
+      option.value = conference.id;
+      option.innerHTML = conference.name;
+      selectTag.appendChild(option);
+    }
+  }
+
+  const formTag = document.getElementById("create-presentation-form");
+  formTag.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(formTag);
+    const json = JSON.stringify(Object.fromEntries(formData));
+    const conferenceSelected = document.getElementById("conference").selectedOptions[0]
+    const presentationUrl = `http://localhost:8000/api/conferences/${conferenceSelected.value}/presentations/`;
+    const fetchConfig = {
+      method: "post",
+      body: json,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(presentationUrl, fetchConfig);
+    if (response.ok) {
+      formTag.reset();
+      const newPresentation = await response.json();
+    }
+  });
+});
